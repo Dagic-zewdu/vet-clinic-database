87f9299e37e7e89edeb35fd10af71714f1ac22a5
Dagi <dagi.zewdu.dz@gmail.com>
2022-04-26 10:48:01 +0300

    Added neutered data to data.sql

--- data.sql
+++ data.sql
@@ -1,7 +1,6 @@
-INSERT INTO animals (name,date_of_birth,weight_kg,escape_attempts) VALUES ('Agumon',"Feb 3rd, 2020",10.23,0);
-INSERT INTO animals (name,date_of_birth,weight_kg,escape_attempts) VALUES ('Gabumon',"Nov 15th, 2018",8,2);
-INSERT INTO animals (name,date_of_birth,weight_kg,escape_attempts) VALUES ('Pikachu',"Jan 7th, 2021,",15.04,1);
-INSERT INTO animals (name,date_of_birth,weight_kg,escape_attempts) VALUES ('Pikachu',"Jan 7th, 2021,",15.04,1);
-INSERT INTO animals (name,date_of_birth,weight_kg,escape_attempts) VALUES ('Devimon',"Jan 7th, 2021,",15.04,1);
+INSERT INTO animals (name,date_of_birth,weight_kg,escape_attempts,neutered) VALUES ('Agumon',"Feb 3rd, 2020",10.23,0,1);
+INSERT INTO animals (name,date_of_birth,weight_kg,escape_attempts,neutered) VALUES ('Gabumon',"Nov 15th, 2018",8,2,1);
+INSERT INTO animals (name,date_of_birth,weight_kg,escape_attempts,neutered) VALUES ('Pikachu',"Jan 7th, 2021,",15.04,0);
+INSERT INTO animals (name,date_of_birth,weight_kg,escape_attempts,neutered) VALUES ('Devimon',"Jan 7th, 2021,",15.04,1);
 
 
