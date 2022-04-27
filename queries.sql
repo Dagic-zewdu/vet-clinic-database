3e870553fbefd0da7b4f20deb4c5bb5a480c9544
Dagi <dagi.zewdu.dz@gmail.com>
2022-04-27 12:17:15 +0300

    Changes through comments

--- queries.sql
+++ queries.sql
@@ -41,31 +41,44 @@ FROM animals
 WHERE weight>=10.4 and weight<=17.3 
 
 /**ex-2*/
-/*Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.*/
-UPDATE animals
-SET species = 'digimon'
-WHERE name LIKE '%mon'
+BEGIN; -- start transaction
 
+   UPDATE animals
+   SET species = 'unspecified'; -- make change
+
+   SELECT species from animals; -- verify that change was made
+   ROLLBACK;
+
+   SELECT species from animals; -- verify that change was undone
+/*Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.*/
 /*Update the animals table by setting the species column to pokemon for all animals that don't have species already set*/
-UPDATE animals
-SET species="pokemon"
-WHERE species IS NULL;
+BEGIN;
+   UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon'; 
+   UPDATE animals SET species = 'pokemon' WHERE species IS NULL; 
+
+   SELECT species from animals; -- verify that change was made
+   COMMIT;
+   SELECT species from animals; -- verify that change persists after commit
 
 /* delete all records in the animals table, then roll back the transaction.*/
-DELETE FROM animals
+BEGIN;
+   DELETE FROM animals WHERE ;
+   SELECT COUNT(*) FROM ANIMALS;
+   ROLLBACK;
+   SELECT COUNT(*) FROM ANIMALS;
 
 /* Delete all animals born after Jan 1st, 2022.*/
-DELETE FROM animals
-WHERE date_of_birth > "01/01/2022"
-
 /*Update all animals' weight to be their weight multiplied by -1*/
-UPDATE animals
-SET weight=weight*(-1)
-
 /*Update all animals' weights that are negative to be their weight multiplied by -1*/
-UPDATE animals
-SET weight=weight*(-1)
-WHERE weight < 0 ;
+BEGIN;
+    DELETE FROM animals WHERE  date_of_birth >= '2022/01/01' ;
+    SAVEPOINT SP1;
+
+    UPDATE animals SET weight_kg = weight_kg * -1; 
+    ROLLBACK TO SP21;
+    UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0; 
+
+    COMMIT;
 
 /*How many animals are there?*/
 SELECT COUNT(id)
@@ -74,7 +87,7 @@ FROM animals;
 /*How many animals have never tried to escape*/
 SELECT COUNT (escape_attempts)
 FROM animals
-WHERE escape_attempts="1"
+WHERE escape_attempts="0"
 
 /*What is the average weight of animals*/
 SELECT AVG(weight)
